a2a5db2c440d2c0776eb77fa474ca71a0dfd4f37
talha915 <talhazafar915426@gmail.com>
2020-07-30 16:07:23 -0700

    Gif List Dynamic

--- VideoPool/src/components/Outro/OutroGif.jsx
+++ VideoPool/src/components/Outro/OutroGif.jsx
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 
+/* Data */
+import outroData from '../../Data/Outro.json';
+
 class OutroGif extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            outroGif: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setOutroGif();
+    }
+
+    setOutroGif=()=> {
+        this.setState({outroGif: outroData.outroGif});
+    }
+
+    getOutroList=()=> {
+        if(this.state.outroGif) {
+            let gifList = this.state.outroGif.map((data, index)=> {
+                return(
+                    <li><a href="#"><img src={data.img} width="200" height="161" alt="image description" /></a></li>
+                )
+            })
+            return gifList;
+        }
+    }
+
     render() {
         return (
             <div className="tab-pane fade show active" id="gif">
@@ -14,11 +44,7 @@ class OutroGif extends Component {
                         </form>
                         <div className="gif-block-wrap">
                             <ul className="gif-bloks-list list-unstyled">
-                                <li><a href="#"><img src="/images/gif01.png" width="200" height="161" alt="image description" /></a></li>
-                                <li><a href="#"><img src="/images/gif03.png" width="200" height="161" alt="image description" /></a></li>
-                                <li><a href="#"><img src="/images/gif01.png" width="200" height="161" alt="image description" /></a></li>
-                                <li><a href="#"><img src="/images/gif03.png" width="200" height="161" alt="image description" /></a></li>
-                                <li><a href="#"><img src="/images/gif01.png" width="200" height="161" alt="image description" /></a></li>
+                                {this.getOutroList()}
                             </ul>
                         </div>
                     </div>
