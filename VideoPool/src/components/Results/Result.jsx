7d4b22f768d687756059da360cdac541e7edcb17
talha915 <talhazafar915426@gmail.com>
2020-07-30 20:44:28 -0700

    Complete Project

--- VideoPool/src/components/Results/Result.jsx
+++ VideoPool/src/components/Results/Result.jsx
@@ -6,6 +6,9 @@ import ResultsData from '../../Data/Results.json';
 /* Router */
 import { withRouter } from 'react-router-dom';
 
+/* Components */
+import Accordion from './Accordion';
+
 class Result extends Component {
 
     constructor(props) {
@@ -28,7 +31,7 @@ class Result extends Component {
             let results = this.state.resultsList.map((data, index)=> {
                 return(
                     <li className="nav-item" key={index}>
-                        <a className={data.activeStatus ? "nav-link active" : "nav-link"} data-toggle="pill" href="#result-vote1" role="tab" aria-selected="true" onClick={()=>{this.resultClick(data, index)}}>
+                        <a className={data.activeStatus ? "nav-link active" : index === 3 ? "nav-link analytics": "nav-link"} data-toggle="pill" href="#result-vote1" role="tab" aria-selected="true" onClick={()=>{this.resultClick(data, index)}}>
                             <span className="align">
                                 <i className="icon icon-vote d-lg-none"></i>
                                 <span className="text d-block">{data.name}</span>
@@ -62,165 +65,25 @@ class Result extends Component {
                                 {this.getResults()}
                             </ul>
                             <div className="inner-tabs-content tab-content">
-                                <div className="tab-pane fade show active" id="result-vote1">
+                                {window.location.pathname === "/results/analytics" ?
+                                    <div className="tab-pane fade show active" id="analytics">
+                                        <div className="inner-tabs-holder w-100">
+                                            <div className="scroll-area sm-scroll">
+                                                <div className="graph-wrap">
+                                                    <img src="/images/graph.jpg" width="640" height="510" alt="graph" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                    :
+                                    <div className="tab-pane fade show active">
                                     <div className="inner-tabs-holder w-100">
                                         <div className="scroll-area">
-                                            <div className="accordion" id="accordion01">
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse01" aria-expanded="false" aria-controls="collapse01">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapse01" className="collapse" data-parent="#accordion01">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse02" aria-expanded="false">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapse02" className="collapse" data-parent="#accordion01">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse03" aria-expanded="false">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapse03" className="collapse" data-parent="#accordion01">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            <Accordion />
                                         </div>
                                     </div>
                                 </div>
-                                <div className="tab-pane fade" id="result-vote2">
-                                    <div className="inner-tabs-holder w-100">
-                                        <div className="scroll-area sm-scroll">
-                                            <div className="accordion" id="accordion02">
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseB01" aria-expanded="false">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapseB01" className="collapse" data-parent="#accordion02">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseB02" aria-expanded="false" >
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapseB02" className="collapse" data-parent="#accordion02">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseB03" aria-expanded="false" >
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapseB03" className="collapse" data-parent="#accordion02">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="tab-pane fade" id="result-vote3">
-                                    <div className="inner-tabs-holder w-100">
-                                        <div className="scroll-area sm-scroll">
-                                            <div className="accordion" id="accordion03">
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseC01" aria-expanded="false">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapseC01" className="collapse" data-parent="#accordionC03">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseC02" aria-expanded="false">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapseC02" className="collapse" data-parent="#accordion01">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="card">
-                                                    <div className="card-header">
-                                                        <h2 className="mb-0">
-                                                            <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseC03" aria-expanded="false">
-                                                                What is Lorem Ipsum? <i className="icon-arrow-long-right"></i>
-                                                            </button>
-                                                        </h2>
-                                                    </div>
-                                                    <div id="collapseC03" className="collapse" data-parent="#accordion03">
-                                                        <div className="card-body">
-                                                            <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="tab-pane fade" id="analytics">
-                                    <div className="inner-tabs-holder w-100">
-                                        <div className="scroll-area sm-scroll">
-                                            <div className="graph-wrap">
-                                                <img src="/images/graph.jpg" width="640" height="510" alt="graph" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            }                             
                             </div>
                         </div>
                     </div>
