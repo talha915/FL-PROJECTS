103b5ed8814b124eae69024cf6e34112344e3955
talha915 <talhazafar915426@gmail.com>
2020-07-31 18:37:59 -0700

    Branding Intro Sound Dynamic Data

--- VideoPool/src/components/Branding/BrandingSound.jsx
+++ VideoPool/src/components/Branding/BrandingSound.jsx
@@ -1,216 +1,61 @@
 import React, { Component } from 'react';
 
+// Scrollbar
 import { Scrollbars } from 'react-custom-scrollbars';
 
+// Data
+import BrandingIntro from '../../Data/Branding.json';
+
 class BrandingSound extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            brandingIntro: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setBrandingSound();
+    }
+
+    setBrandingSound=()=> {
+        this.setState({brandingIntro: BrandingIntro.brandingSound});
+    }
+
+    getBrandSoundList=()=> {
+        if(this.state.brandingIntro) {
+            let list = this.state.brandingIntro.map((data, index)=> {
+                return(
+                    <li>
+                        <div className="sound-leftsize">
+                            <div className="icon-wrap">
+                                <a href="#">
+                                    <img src={data.img} width="45" height="45" alt="play button" />
+                                </a>
+                            </div>
+                            <div className="sound-detail">
+                                <strong className="title">{data.heading1}</strong>
+                                <span className="subtitle">{data.heading2}</span>
+                            </div>
+                        </div>
+                        <a href="#" className="btn btn-warning rounded-btn">{data.btn}</a>
+                    </li>
+                )
+            })
+            return list;
+        }
+    }
+
     render() {
         return (
             <div className="tab-pane fade show active" id="logo-sound">
                 <div className="inner-tabs-holder w-100">
                     <Scrollbars className="scroll-area sm-scroll">
                         <ul className="intro-sound-list list-unstyled">
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
-                            <li>
-                                <div className="sound-leftsize">
-                                    <div className="icon-wrap">
-                                        <a href="#">
-                                            <img src="/images/play.png" width="45" height="45" alt="play button" />
-                                        </a>
-                                    </div>
-                                    <div className="sound-detail">
-                                        <strong className="title">Then you k...</strong>
-                                        <span className="subtitle">Snow Dept.</span>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-warning rounded-btn">Use</a>
-                            </li>
+                            {this.getBrandSoundList()}
                         </ul>
                     </Scrollbars>
-                
-                
-                
-                
                 </div>
             </div>
         )
