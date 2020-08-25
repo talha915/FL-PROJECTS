e91cb4022bcd92e9e7ec4dfc0771bb25375478ae
talha915 <talhazafar915426@gmail.com>
2020-08-26 00:50:04 +0500

    Video full complete

--- video-pool-mobile/src/components/Header.jsx
+++ video-pool-mobile/src/components/Header.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import '../../src/styles/main.css';
 
+/* Router */
+import {withRouter} from 'react-router-dom';
+
 class Header extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			router: ''
+		}
+	}
+
+	componentWillReceiveProps() {
+		let routes = this.props.routeProps;
+		this.setState({router: routes});
+	}
+
+	backRoute=()=> {
+		this.props.history.push('/');
+	}
+
     render() {
+		
         return(
             <header id="header">
 				<div className="container-fluid">
 					<div className="d-flex align-items-center justify-content-between">
 						<div className="logo order-2 order-md-1"><a href="#"><span className="logo-text">Project Name</span></a></div>
+						{this.state.router == 0 ? <a className="btn-back" href="#" onClick={this.backRoute()}><i className="icon-arrow-back"></i></a>: ''}
 					</div>
 				</div>
 			</header>
@@ -15,4 +37,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default withRouter(Header);
--- video-pool-mobile/src/components/Video.jsx
+++ video-pool-mobile/src/components/Video.jsx
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 import Videos from './Video/Videos';
 import Header from './Header';
 class Video extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            routes: 0
+        }
+    }
     render() {
         return (
             <div id="page">
                 <div className="dashboard-page">
-                    <Header />
+                    <Header routeProps={this.state.routes}/>
                     <Videos />
                 </div>
             </div>
