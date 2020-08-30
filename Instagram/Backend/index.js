188785049790462c3f8644debb59df9f715f7761
talha915 <talhazafar915426@gmail.com>
2020-08-30 19:47:04 +0500

    Index js Working

--- Instagram/Backend/index.js
+++ Instagram/Backend/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const customMiddleware = (req, res, next)=> {
+    console.log("Middleware executed!")
+    next()
+}
+
 app.get('/', (req, res)=> {
     res.send('Hello World');
+})
+
+app.get('/about', customMiddleware, (req, res)=> {
+    console.log("ABout");
+    res.send('About Page');
 })
 
 app.listen(PORT, ()=> {
