e1002602580e253c2f202987d3655eb0217e0edb
talha915 <talhazafar915426@gmail.com>
2020-09-20 16:26:25 +0500

    Header dynamic fixed

--- VideoPool/src/components/Workspace/Header/Header.jsx
+++ VideoPool/src/components/Workspace/Header/Header.jsx
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 
+// HeaderData
+import HeaderData from '../Data/Header.json';
+
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            headerData: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setHeader();
+    }
+
+    setHeader=()=> {
+        this.setState({headerData: HeaderData.header})
+    }
+
+    getHeaderList=()=> {
+        if(this.state.headerData) {
+            console.log("Window loc: ", window.location.pathname)
+            let headerList = this.state.headerData.navList.map((data, index)=> {
+                return(
+                    <li key={index} className={window.location.pathname === data.path ? "active" : ""}><a href="#">{data.name}</a></li>
+                )
+            })
+            return headerList;
+        }
+    }
+
     render() {
         return(
             <div className="main-header">
@@ -8,9 +39,10 @@ class Header extends Component {
 					<div className="holder d-flex align-items-center">
 						<div className="logo"><a href="#"><span className="logo-text">My Workspace</span></a></div>
 						<ul className="main-nav">
-							<li><a href="#">Project</a></li>
+							{/* <li><a href="#">Project</a></li>
 							<li className="active"><a href="#">Branding</a></li>
-							<li><a href="#">Setting</a></li>
+							<li><a href="#">Setting</a></li> */}
+                            {this.getHeaderList()}
 						</ul>
 					</div>
 					<div className="btn-holder d-flex align-items-center">
