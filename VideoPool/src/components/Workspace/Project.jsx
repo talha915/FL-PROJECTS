f4d309b33757225bbf15faf8f18f18e36976ff51
talha915 <talhazafar915426@gmail.com>
2020-09-20 17:09:17 +0500

    Project Added

--- VideoPool/src/components/Workspace/Project.jsx
+++ VideoPool/src/components/Workspace/Project.jsx
@@ -7,32 +7,24 @@ import Header from './Header/Header';
 class Project extends Component {
     render() {
         return (
-            <body className="bg-white">
-                <div className="workspace-page">
-                    <Sidebar />
-                    <div className="right-content">
-                        <Header />
-                        <main>
-                            <div className="content-wrap">
-                                <div className="project-holder">
-                                    <div className="column">
-                                        <div className="img-box">
-                                            <a href="#">
-                                                <img src="/images/img3.jpg" width="325" height="325" alt="img description" />
-                                            </a>
-                                        </div>
-                                        <div className="text-box">
-                                            <a className="more" href="#"></a>
-                                            <strong className="title">Project Name</strong>
-                                            <time className="date" dateTime="2008-02-14">4/4/2020</time>
-                                        </div>
-                                    </div>
-                                </div>
+            <main>
+                <div className="content-wrap">
+                    <div className="project-holder">
+                        <div className="column">
+                            <div className="img-box">
+                                <a href="#">
+                                    <img src="/images/img3.jpg" width="325" height="325" alt="img description" />
+                                </a>
                             </div>
-                        </main>
+                            <div className="text-box">
+                                <a className="more" href="#"></a>
+                                <strong className="title">Project Name</strong>
+                                <time className="date" dateTime="2008-02-14">4/4/2020</time>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </body>
+            </main>
         )
     }
 }
