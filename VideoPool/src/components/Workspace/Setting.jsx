fa73ac73838af0342fa537211c53b02a592150b8
talha915 <talhazafar915426@gmail.com>
2020-09-20 17:16:54 +0500

    Setting updated

--- VideoPool/src/components/Workspace/Setting.jsx
+++ VideoPool/src/components/Workspace/Setting.jsx
@@ -4,82 +4,82 @@ class Setting extends Component {
     render() {
         return(
             <main>
-				<div class="content-wrap">
-					<form action="#" class="form-info">
-						<div class="content-group">
-							<div class="content-row">
-								<div class="text-wrap">
-									<strong class="subtitle">Workspace Name</strong>
-									<div class="text-area">My Workspace</div>
+				<div className="content-wrap">
+					<form action="#" className="form-info">
+						<div className="content-group">
+							<div className="content-row">
+								<div className="text-wrap">
+									<strong className="subtitle">Workspace Name</strong>
+									<div className="text-area">My Workspace</div>
 								</div>
-								<a href="#" class="link">Edit</a>
+								<a href="#" className="link">Edit</a>
 							</div>
 						</div>
-						<div class="content-group">
-							<strong class="title">Collaborators</strong>
-							<div class="content-row">
-								<div class="text-wrap">
-									<strong class="subtitle">Invite by email</strong>
+						<div className="content-group">
+							<strong className="title">Collaborators</strong>
+							<div className="content-row">
+								<div className="text-wrap">
+									<strong className="subtitle">Invite by email</strong>
 								</div>
-								<a href="#" class="btn btn-primary">Invite</a>
+								<a href="#" className="btn btn-primary">Invite</a>
 							</div>
-							<div class="content-row">
-								<div class="text-wrap">
-									<div class="user-box">
-										<div class="img-box">
+							<div className="content-row">
+								<div className="text-wrap">
+									<div className="user-box">
+										<div className="img-box">
 											<span>FX</span>
 										</div>
-										<div class="text-box">
-											<strong class="name">Enter Name</strong>
-											<a class="mail" href="#">foxylady@gmail.com</a>
+										<div className="text-box">
+											<strong className="name">Enter Name</strong>
+											<a className="mail" href="#">foxylady@gmail.com</a>
 										</div>
 									</div>
 								</div>
-								<a href="#" class="link">Admin</a>
+								<a href="#" className="link">Admin</a>
 							</div>
 						</div>
-						<div class="content-group">
-							<strong class="title">Personal/Company</strong>
-							<div class="content-row input-row justify-content-start">
-								<label for="fname">First Name :</label>
-								<div class="input-box">
+						<div className="content-group">
+							<strong className="title">Personal/Company</strong>
+							<div className="content-row input-row justify-content-start">
+								<label htmlFor="fname">First Name :</label>
+								<div className="input-box">
 									<input id="fname" type="text" />
 								</div>
 							</div>
-							<div class="content-row input-row justify-content-start">
-								<label for="lname">Last Name :</label>
-								<div class="input-box">
+							<div className="content-row input-row justify-content-start">
+								<label htmlFor="lname">Last Name :</label>
+								<div className="input-box">
 									<input id="lname" type="text" />
 								</div>
 							</div>
-							<div class="content-row input-row justify-content-start">
+							<div className="content-row input-row justify-content-start">
 								<label>Photo :</label>
-								<div class="photo-box">
-									<i class="icon-tick"></i>
-									<div class="photo"><img src="/images/img04.png" width="53" height="52" alt="img description" /></div>
-									<label class="btn-photo">
-										<input class="file" type="file" />
+								<div className="photo-box">
+									<i className="icon-tick"></i>
+									<div className="photo"><img src="/images/img04.png" width="53" height="52" alt="img description" /></div>
+									<label className="btn-photo">
+										<input className="file" type="file" />
 										change photo
 									</label>
 								</div>
 							</div>
-							<div class="content-row input-row justify-content-start">
-								<label for="company">Company Name :</label>
-								<div class="input-box">
+							<div className="content-row input-row justify-content-start">
+								<label htmlFor="company">Company Name :</label>
+								<div className="input-box">
 									<input id="company" type="text" />
 								</div>
 							</div>
 						</div>
-						<div class="content-group">
-							<strong class="title">Delete Workspace</strong>
-							<div class="content-row">
-								<div class="text-wrap">
-									<div class="text-area">
-										<strong class="subtitle">Delete your space "Workspace"</strong>
+						<div className="content-group">
+							<strong className="title">Delete Workspace</strong>
+							<div className="content-row">
+								<div className="text-wrap">
+									<div className="text-area">
+										<strong className="subtitle">Delete your space "Workspace"</strong>
 										<p>This will delete your work and all content within it and remove all team members from it</p>
 									</div>
 								</div>
-								<a href="#" class="link">Delete Workspace</a>
+								<a href="#" className="link">Delete Workspace</a>
 							</div>
 						</div>
 					</form>
