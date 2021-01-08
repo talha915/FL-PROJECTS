29ea2a5ebd0f7d3c0e8069915f8f8d021b2c2a22
talha915 <talhazafar915426@gmail.com>
2021-01-08 14:11:21 +0500

    Dynamically Started

--- Tunnins/tunnin-web-app/tunnins-web-app/src/components/Main.jsx
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/components/Main.jsx
@@ -3,11 +3,40 @@ import React, { Component } from 'react';
 // Components
 import Login from './Login';
 
+// Data
+import Data from '../Data/Data.json';
+
 class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setData();
+    }
+
+    setData = () => {
+        this.setState({ data: Data });
+    }
+
+    getData = () => {
+        console.log("Data: ", this.state.data);
+        if (this.state.data) {
+            let data = this.state.data;
+            return (
+                <Login loginDetails={data} />
+            )
+        }
+    }
+
     render() {
-        return(
+        return (
             <div>
-                <Login />
+                {this.getData()}
             </div>
         )
     }
