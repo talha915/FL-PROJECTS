b1932e8d14ae5bd78f60c97d891397446530090e
talha915 <talhazafar915426@gmail.com>
2021-01-08 13:56:00 +0500

    Left section completed

--- Tunnins/tunnin-web-app/tunnins-web-app/src/components/Login.jsx
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/components/Login.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
 
-import '../styles/login.scss';
+import '../styles/login.css';
 
 function Login() {
     return (
         <div className="login-container">
             <Row>
-                <Col md="6" sm="4">
-                    Tunnin
+                <Col md="6" sm="4" className="left-login">
+                    <h3 className="login-logo">
+                        Tunnin
+                    </h3>
                 </Col>
                 <Col md="6" sm="4">
                     Login
