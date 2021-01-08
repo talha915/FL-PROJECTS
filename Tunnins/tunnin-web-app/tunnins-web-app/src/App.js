e9c7847c650dff36c1adf3b19b0baf72d1845665
talha915 <talhazafar915426@gmail.com>
2021-01-08 12:01:23 +0500

    Tunnin Web App

---
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Components
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
