4151dee1d484dc055aa1a2ae2d19340f698f55ff
talha915 <talhazafar915426@gmail.com>
2021-01-16 21:08:54 +0500

    Redux features added

--- Tunnins/tunnin-web-app/tunnins-web-app/src/App.js
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/App.js
@@ -3,6 +3,12 @@ import React from 'react';
 // Components
 import Main from './components/Main';
 
+// Redux
+import {applyMiddleware, createStore} from 'redux';
+import {logger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import {Provider} from 'react-redux';
+
 function App() {
   return (
     <div>
