615d715301da8b7d218b77b4a24ec63fd5812b57
talha915 <talhazafar915426@gmail.com>
2021-01-16 13:04:57 +0500

    Adding reducer

---
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/reducer/getDataReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    dataState = []
+}
+
+export default function(state=initialState, actions) {
+    switch(actions.type) {
+        case 'fetchLocalData':
+            return [
+                ...state,
+                ...actions.payload
+            ]
+        default:
+            return state;    
+    }
+}
