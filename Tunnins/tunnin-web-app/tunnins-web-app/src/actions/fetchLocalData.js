0c280a4e2cf12d8b51d8a6259d8ed737957d42d5
talha915 <talhazafar915426@gmail.com>
2021-01-16 21:47:13 +0500

    Action Added

---
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/actions/fetchLocalData.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+import localData from '../Data/Data.json';
+
+export const getLocal=()=>{
+    return (dispatch)=> {
+        axios.get(localData)
+        .then((res)=> {
+            dispatch({
+                type: 'fetchLocalData',
+                data: res
+            })
+        })
+    }
+}
