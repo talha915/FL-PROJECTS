adde1829048f01344140ea8d8eebbfac273eaf0e
talha915 <talhazafar915426@gmail.com>
2021-01-18 04:43:39 +0500

    Router Added

--- Tunnins/tunnin-web-app/tunnins-web-app/src/index.js
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/index.js
@@ -7,13 +7,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://localhost:3000/";
 
 ReactDOM.render(
   <React.Fragment>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.Fragment>,
   document.getElementById('root')
 );
