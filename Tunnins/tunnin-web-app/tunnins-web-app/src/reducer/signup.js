8a16b4d07216e39148e807499302b20a08bbbd62
talha915 <talhazafar915426@gmail.com>
2021-01-18 18:17:48 +0500

    Reducer for signup

---
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/reducer/signup.js
@@ -0,0 +1,20 @@
+// Constants
+import sign_up from '../constants/constants';
+
+const initialState = {
+    signUp: {
+
+    } 
+}
+
+export default function(state=initialState, actions) {
+    switch(actions.type) {
+        case sign_up:
+            return {
+                ...signUp,
+                data: actions.payload
+            }
+        default:
+            return state;    
+    }
+}
