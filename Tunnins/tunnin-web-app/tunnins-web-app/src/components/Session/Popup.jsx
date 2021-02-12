8a93de3c63c48b469c14b7ad0c82f4d285c54fb8
talha915 <talhazafar915426@gmail.com>
2021-02-13 01:47:38 +0500

    All fixes

--- Tunnins/tunnin-web-app/tunnins-web-app/src/components/Session/Popup.jsx
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/components/Session/Popup.jsx
@@ -10,20 +10,37 @@ import { sessionModal } from '../../actions/sessionModal';
 import { withRouter } from 'react-router-dom';
 
 // Constants
-import { close_modal, add_session_modal } from '../../constants/constants';
+import { close_modal, add_session_modal, cancelled_session_modal } from '../../constants/constants';
 
 
 function Popup(props) {
 
     const dispatch = useDispatch();
 
-    
+    const [cancelAction, cancelled] = useState('');    
 
     const modalOperate=(item)=> {
-        dispatch(sessionModal(close_modal));
-        if(props.action_type == add_session_modal) {
-            let routeTo = item.route;
-            props.history.push(routeTo);
+        console.log("Item: ", item);
+        if(item.hasOwnProperty("action")) {
+            if(item.action === "yes") {
+                dispatch(sessionModal(close_modal));
+                cancelled("yes");
+                dispatch(sessionModal(cancelled_session_modal));
+            }
+            else {
+                dispatch(sessionModal(close_modal));
+            }
+        }
+        else if(item.hasOwnProperty("route")) {
+            dispatch(sessionModal(close_modal));
+            props.history.push(item.route);
+        }
+        else {
+            dispatch(sessionModal(close_modal));
+            if(props.action_type == add_session_modal) {
+                let routeTo = item.route;
+                props.history.push(routeTo);
+            }
         }
     }
 
@@ -38,25 +55,34 @@ function Popup(props) {
         return btn;
     }
 
+    const popup = useSelector(state => state.sessionModal);
+
+    const getModal=()=> {  
+        return(
+            <Modal isOpen={popup.hasOwnProperty("popUp") ? popup.popUp : props.modalState.popUp}>
+                <ModalHeader>
+                    <div>
+                        <h6>
+                            {popup.hasOwnProperty("title") ? popup.title : props.modalState.modalState.title}
+                        </h6>
+                    </div>
+                </ModalHeader>
+                <ModalBody>
+                    <p>
+                        {popup.hasOwnProperty("content") ? popup.content : props.modalState.modalState.content}
+                    </p>
+                </ModalBody>
+                <ModalFooter>
+                    {getButton(popup.hasOwnProperty("btn") ? popup.btn : props.modalState.modalState)}
+                </ModalFooter>
+            </Modal>
+        );      
+    }
 
     return(
-        <Modal isOpen={props.modalState.popUp}>
-            <ModalHeader>
-                <div>
-                    <h6>
-                        {props.modalState.modalState.title}
-                    </h6>
-                </div>          
-            </ModalHeader>
-            <ModalBody>
-                <p>
-                    {props.modalState.modalState.content}
-                </p>
-            </ModalBody>
-            <ModalFooter>
-                {getButton(props.modalState.modalState)}
-            </ModalFooter>
-        </Modal>
+        <div>
+            {getModal()}
+        </div>
     )
 }
 
