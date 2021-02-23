267b76056a546e8682e50ab78ff3cb849bf31f86
talha915 <talhazafar915426@gmail.com>
2021-02-23 16:31:16 +0500

    Earnings finish

--- Tunnins/tunnin-web-app/tunnins-web-app/src/components/MyEarnings/Earnings.jsx
+++ Tunnins/tunnin-web-app/tunnins-web-app/src/components/MyEarnings/Earnings.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Form, FormGroup, Label, Input, Row, Col, Card, CardText, CardBody, CardLink,
-    CardTitle, CardSubtitle } from 'reactstrap';
+import React, { useEffect } from 'react';
+import { Table, Row, Col, Card, CardText, 
+    CardTitle} from 'reactstrap';
 
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 
 // Action
-import { ListNotification } from '../../actions/notification';
+import { fetchEarning } from '../../actions/earnings';
 
 // Router
 import { withRouter } from 'react-router-dom';
 
 // Constants
-import { listed_notification } from '../../constants/constants';
+import { getEarnings } from '../../constants/constants';
 
 // Styles
 import '../../styles/earnings.scss';
@@ -22,6 +22,93 @@ import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
 
 function Earnings() {
+
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatchEarnings();
+    }, []);
+
+    const dispatchEarnings=()=> {
+        dispatch(fetchEarning(getEarnings));
+    }
+
+    const getEarn = useSelector(state => state.earnings);
+
+    console.log("Earn: ", getEarn);
+
+    const getUpperPart=()=> {
+        if(getEarn.hasOwnProperty('data')) {
+            let earnings = getEarn.data;
+            return(
+                <Row>
+                    <Col>
+                        {earnings.heading}
+                    </Col>
+                    {getUpperCards(earnings.upperCards)}
+                </Row>
+            )
+        }
+    }
+
+    const getUpperCards=(data)=> {
+        let cards = data.map((items, index)=> {
+            return(
+                <Col key={index}>
+                    <Card body className="card-style" >
+                        <div className="card-content">
+                            <div>
+                                <CardTitle tag="h5">{items.price}</CardTitle>
+                                <CardText>{items.title}</CardText>
+                            </div>
+                        </div>
+                    </Card>
+                </Col>
+            )
+        });
+        return cards;
+    }
+
+    const getTableHeaders=()=> {
+        if(getEarn.hasOwnProperty('data')) {
+            let tableHeaders = getEarn.data.tableHeader.map((data, index)=> {
+                return (
+                    <th key={index}>
+                        {data.title}
+                    </th>
+                )
+            });
+            return tableHeaders;
+        }
+    }
+
+    const getTableValues=()=> {
+        if(getEarn.hasOwnProperty('data')) {
+            let getValues = getEarn.data.tableValues.map((data, index)=>{
+                return (
+                    <tr key={index}>
+                        <th>
+                            {data.nameofSession}
+                        </th>
+                        <th>
+                            {data.date}
+                        </th>
+                        <th>
+                            {data.time}
+                        </th>
+                        <th>
+                            {data.users}
+                        </th>
+                        <th>
+                            {data.earnings}
+                        </th>
+                    </tr>
+                )
+            });
+            return getValues;
+        }
+    }
+
     return(
         <div className="earning">
             <Header />
@@ -32,10 +119,19 @@ function Earnings() {
                     </Col>
                     <Col sm="9" className="custom-offset">
                         <div className="session-btn-wrapper">
-                            
+                            {getUpperPart()}
                         </div>
                         <div className="session-cards-wrapper">
-                            
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        {getTableHeaders()}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {getTableValues()}
+                                </tbody>
+                            </Table>
                         </div>      
                     </Col>
                 </Row>
