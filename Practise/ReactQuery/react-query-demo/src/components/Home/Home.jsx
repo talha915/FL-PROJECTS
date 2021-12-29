1fd04f1fd0310018204f7fe45f41b9c4ea714586
unknown <talhazafar915426@gmail.com>
2021-12-29 17:16:25 +0500

    New

---
+++ Practise/ReactQuery/react-query-demo/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import '../../App.css';
+
+const Home = () => {
+    return (
+        <div className="App">
+            <h1>Star Wars Info</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ Practise/ReactQuery/react-query-demo/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar=()=> {
+    return(
+        <nav>
+            <button>
+                Planets
+            </button>
+            <button>
+                People
+            </button>
+        </nav>
+    )
+}
+
+export const Navbar;
