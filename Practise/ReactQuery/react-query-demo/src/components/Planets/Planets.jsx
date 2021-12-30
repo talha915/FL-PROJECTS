c4c68ef73d676e23b80cf9a15308f4df7778d196
unknown <talhazafar915426@gmail.com>
2021-12-31 01:37:20 +0500

    New

--- Practise/ReactQuery/react-query-demo/src/components/Planets/Planets.jsx
+++ Practise/ReactQuery/react-query-demo/src/components/Planets/Planets.jsx
@@ -1,4 +1,15 @@
+import { useQuery } from 'react-query';
+
+const fetchPlanets = async()=> {
+    const res = await fetch('https://swapi.dev/api/planets/');
+    return res.json();
+}
+
 const Planets=()=> {
+
+    const {data, status} = useQuery('planets', fetchPlanets);
+    console.log(data);
+
     return(
         <div>
             Planets
