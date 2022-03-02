a585c54d0e2ab95c141056a840efef78c7d9e94b
unknown <talhazafar915426@gmail.com>
2022-03-02 14:48:17 +0500

    Array_types Types

---
+++ TypeScript Practise/fetchjson/Array_types.ts
@@ -0,0 +1,20 @@
+const cars: string[] = ['ford', 'bmw', 'toyota', 'honda'];
+
+//const myCars = cars;
+const myCars = [...cars];
+
+myCars.push('suzuki');
+
+//myCars.pop();
+
+console.log(`My Cars: ${myCars} and real cars: ${cars}`);
+
+myCars.map((data: string, index: number): void=> {
+    console.log(`Data: ${data} & index: ${index}`);
+});
+
+const newCars = myCars.map((data: string, index: number): string=> {
+    return data.toUpperCase();
+});
+
+console.log("NEW CARS: ", newCars);
