7dd157a6d8f78d86aa637aa18813f10a8a438f0e
unknown <talhazafar915426@gmail.com>
2022-03-02 12:37:26 +0500

    TypeScript Practise Axios

--- TypeScript Practise/fetchjson/index.ts
+++ TypeScript Practise/fetchjson/index.ts
@@ -3,5 +3,14 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(res=>{
-    console.log(res.data);
+    const todo = res.data;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        Id: ${id},
+        Title: ${title},
+        Finished?: ${completed}
+    `);
 })
